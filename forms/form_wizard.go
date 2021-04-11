120c49733dd4b6bcb5d78197a176631e4df3ad76
ipoluyanov <ipoluyanov@gmail.com>
2021-04-11 15:21:16 +0300

    setup wizard

--- forms/form_wizard.go
+++ forms/form_wizard.go
@@ -2,9 +2,11 @@ package forms
 
 import (
 	"github.com/gazercloud/gazernode/client"
+	"github.com/gazercloud/gazerui/ui"
 	"github.com/gazercloud/gazerui/uicontrols"
 	"github.com/gazercloud/gazerui/uievents"
 	"github.com/gazercloud/gazerui/uiinterfaces"
+	"github.com/gazercloud/gazerui/uiresources"
 )
 
 type FormWizard struct {
@@ -22,18 +24,16 @@ func NewFormWizard(parent uiinterfaces.Widget, client *client.Client) *FormWizar
 func (c *FormWizard) OnInit() {
 	c.Dialog.OnInit()
 	c.SetTitle("Setup Wizard")
-	c.Resize(900, 500)
+	c.Resize(500, 400)
 
 	pContent := c.ContentPanel().AddPanelOnGrid(0, 0)
 	pButtons := c.ContentPanel().AddPanelOnGrid(0, 1)
 
-	pContent.AddButtonOnGrid(0, 0, "Ping to gazer.cloud", func(event *uievents.Event) {
-		c.client.AddUnit("network_ping", "Ping to gazer.cloud", `{"addr": "gazer.cloud","frame_size": 64,"period": 1000,"timeout": 1000}`, func(s string, err error) {
-			c.Accept()
-		})
-	})
 	// {"addr": "localhost","frame_size": 64,"period": 1000,"timeout": 1000}
-	pContent.AddButtonOnGrid(1, 0, "Operation system", func(event *uievents.Event) {
+
+	var btn *uicontrols.Button
+
+	btn = pContent.AddButtonOnGrid(0, 0, "OS Resources\r\nMonitoring", func(event *uievents.Event) {
 		c.client.AddUnit("windows_memory", "Windows Memory", "", func(s string, err error) {
 			c.client.AddUnit("windows_network", "Windows Network", "", func(s string, err error) {
 				c.client.AddUnit("windows_storage", "Windows Storage", "", func(s string, err error) {
@@ -44,11 +44,36 @@ func (c *FormWizard) OnInit() {
 			})
 		})
 	})
-	// {"period": 1000,"process_name": "explorer.exe"}
-	pContent.AddButtonOnGrid(0, 1, "Network", func(event *uievents.Event) {
+	btn.SetImage(uiresources.ResImgCol(uiresources.R_icons_material4_png_hardware_computer_materialiconsoutlined_48dp_1x_outline_computer_black_48dp_png, c.ForeColor()))
+	btn.SetImageSize(64, 64)
+	btn.SetMouseCursor(ui.MouseCursorPointer)
+
+	btn = pContent.AddButtonOnGrid(1, 0, "Memory\r\nMonitoring", func(event *uievents.Event) {
+		c.client.AddUnit("windows_memory", "Windows Memory", "", func(s string, err error) {
+			c.Accept()
+		})
 	})
-	pContent.AddButtonOnGrid(1, 1, "Process", func(event *uievents.Event) {
+	btn.SetImage(uiresources.ResImgCol(uiresources.R_icons_material4_png_hardware_memory_materialiconsoutlined_48dp_1x_outline_memory_black_48dp_png, c.ForeColor()))
+	btn.SetImageSize(64, 64)
+	btn.SetMouseCursor(ui.MouseCursorPointer)
+
+	btn = pContent.AddButtonOnGrid(0, 1, "Networks\r\nMonitoring", func(event *uievents.Event) {
+		c.client.AddUnit("windows_network", "Windows Networks", "", func(s string, err error) {
+			c.Accept()
+		})
 	})
+	btn.SetImage(uiresources.ResImgCol(uiresources.R_icons_material4_png_notification_network_check_materialiconsoutlined_48dp_1x_outline_network_check_black_48dp_png, c.ForeColor()))
+	btn.SetImageSize(64, 64)
+	btn.SetMouseCursor(ui.MouseCursorPointer)
+
+	btn = pContent.AddButtonOnGrid(1, 1, "Storage\r\nMonitoring", func(event *uievents.Event) {
+		c.client.AddUnit("windows_storage", "Windows Storage", "", func(s string, err error) {
+			c.Accept()
+		})
+	})
+	btn.SetImage(uiresources.ResImgCol(uiresources.R_icons_material4_png_device_storage_materialiconsoutlined_48dp_1x_outline_storage_black_48dp_png, c.ForeColor()))
+	btn.SetImageSize(64, 64)
+	btn.SetMouseCursor(ui.MouseCursorPointer)
 
 	pContent.AddVSpacerOnGrid(0, 2)
 
