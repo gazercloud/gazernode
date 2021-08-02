892023e32bc56029cd4639e7bf5959eeac1355d8
ipoluyanov <ipoluyanov@gmail.com>
2021-08-02 14:33:59 +0300

    mac sync

---
+++ system/units/windows/unit_network_interface/unit_network_interface_linux.go
@@ -0,0 +1,42 @@
+package unit_network_interface
+
+import (
+	"github.com/gazercloud/gazernode/common_interfaces"
+	"github.com/gazercloud/gazernode/resources"
+	"github.com/gazercloud/gazernode/system/units/units_common"
+)
+
+type UnitNetworkInterface struct {
+	units_common.Unit
+
+	interfaceName string
+}
+
+var Image []byte
+
+func init() {
+	Image = resources.R_files_sensors_sensor_windows_ram_png
+}
+
+func New() common_interfaces.IUnit {
+	var c UnitNetworkInterface
+	return &c
+}
+
+func (c *UnitNetworkInterface) GetConfigMeta() string {
+	meta := units_common.NewUnitConfigItem("", "", "", "", "", "", "")
+	meta.Add("interface_name", "Network Interface", "-", "string", "", "", "network_interface")
+	meta.Add("period", "Period, ms", "1000", "num", "0", "999999", "")
+	return meta.Marshal()
+}
+
+func (c *UnitNetworkInterface) InternalUnitStart() error {
+	go c.Tick()
+	return nil
+}
+
+func (c *UnitNetworkInterface) InternalUnitStop() {
+}
+
+func (c *UnitNetworkInterface) Tick() {
+}
