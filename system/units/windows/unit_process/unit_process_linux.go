49e8e29b5c1c23014d03e9657834a679d548661d
ipoluyanov <ipoluyanov@gmail.com>
2021-08-09 08:45:26 +0300

    mac sync

--- system/units/windows/unit_process/unit_process_linux.go
+++ system/units/windows/unit_process/unit_process_linux.go
@@ -1,20 +1,93 @@
 package unit_process
 
 import (
+	"encoding/json"
 	"errors"
-	"fmt"
-	"github.com/shirou/gopsutil/process"
+	"github.com/gazercloud/gazernode/logger"
+	"github.com/gazercloud/gazernode/resources"
+	"strconv"
+	"syscall"
 	"time"
 )
 
 var Image []byte
 
 func init() {
+	Image = resources.R_files_sensors_sensor_windows_proc_png
 }
 
 func (c *UnitSystemProcess) InternalUnitStart() error {
+	var err error
+	type Config struct {
+		ProcessName string  `json:"process_name"`
+		Period      float64 `json:"period"`
+	}
+
+	c.SetMainItem("Main/Working Set Size")
+
+	{
+		// Common
+		c.SetString("Common/Name", "", "")
+		c.SetString("Common/ProcessID", "", "")
+	}
+
+	var config Config
+	err = json.Unmarshal([]byte(c.GetConfig()), &config)
+	if err != nil {
+		logger.Println("ERROR[UnitSystemProcess]:", err)
+		err = errors.New("config error")
+		c.SetString("Common/ProcessID", err.Error(), "error")
+		return err
+	}
+
+	runes := []rune(config.ProcessName)
+	indexOfSlash := -1
+	for i, r := range runes {
+		if r == '#' {
+			indexOfSlash = i
+		}
+	}
+
+	if indexOfSlash > -1 {
+		var prId uint64
+		prId, err = strconv.ParseUint(string(runes[indexOfSlash+1:]), 10, 32)
+		if err == nil {
+			c.processId = uint32(prId)
+			c.processIdActive = true
+		} else {
+			c.processId = 0
+			c.processIdActive = false
+		}
+
+		c.processNameActive = false
+		c.processName = ""
+
+		if indexOfSlash > 0 {
+			c.processNameActive = true
+			c.processName = string(runes[:indexOfSlash])
+		}
+	} else {
+		c.processIdActive = false
+		c.processId = 0
+		c.processNameActive = true
+		c.processName = config.ProcessName
+	}
+
+	if !c.processIdActive && !c.processNameActive {
+		err = errors.New("wrong filter")
+		c.SetString("Common/ProcessID", err.Error(), "error")
+		return err
+	}
+
+	c.periodMs = int(config.Period)
+	if c.periodMs < 100 {
+		err = errors.New("wrong period")
+		c.SetString("Common/ProcessID", err.Error(), "error")
+		return err
+	}
+
 	go c.Tick()
-	return errors.New("not implemented")
+	return nil
 }
 
 func (c *UnitSystemProcess) InternalUnitStop() {
@@ -22,34 +95,59 @@ func (c *UnitSystemProcess) InternalUnitStop() {
 
 func (c *UnitSystemProcess) Tick() {
 	c.Started = true
+	logger.Println("UNIT <Process Windows> started:", c.Id())
+
+	dtOperationTime := time.Now().UTC()
+
+	processId := int32(-1)
+
+	lastKernelTimeMs := int64(0)
+	lastUserTimeMs := int64(0)
+	lastReadProcessTimes := time.Now().UTC()
 
 	for !c.Stopping {
-		time.Sleep(1000 * time.Millisecond)
-		found := false
-
-		processes, _ := process.Processes()
-		for _, pr := range processes {
-			name, err := pr.Name()
-			if err == nil {
-				if name == "chrome.exe" {
-					found = true
-					break
-				}
+		for {
+			if c.Stopping || time.Now().Sub(dtOperationTime) > time.Duration(c.periodMs)*time.Millisecond {
+				break
 			}
+			time.Sleep(10 * time.Millisecond)
+		}
+		if c.Stopping {
+			break
 		}
 
-		if !found {
-			c.SetString("error", "no process found", "")
-		} else {
-			c.SetString("error", "", "")
+		var ru syscall.Rusage
+
+		err = syscall.Getrusage(0, &ru)
+
+		if err != nil {
+			// Common
+			c.SetInt64("Maxrss", ru.Maxrss, "")
+			c.SetInt64("Ixrss", ru.Ixrss, "")
+			c.SetInt64("Idrss", ru.Idrss, "")
+			c.SetInt64("Isrss", ru.Isrss, "")
+			c.SetInt64("Minflt", ru.Minflt, "")
+			c.SetInt64("Majflt", ru.Majflt, "")
+			c.SetInt64("Nswap", ru.Nswap, "")
+			c.SetInt64("Inblock", ru.Inblock, "")
+			c.SetInt64("Oublock", ru.Oublock, "")
+			c.SetInt64("Msgsnd", ru.Msgsnd, "")
+			c.SetInt64("Msgrcv", ru.Msgrcv, "")
+			c.SetInt64("Nsignals", ru.Nsignals, "")
+			c.SetInt64("Nvcsw", ru.Nvcsw, "")
+			c.SetInt64("Nivcsw", ru.Nivcsw, "")
 		}
+
+		dtOperationTime = time.Now().UTC()
 	}
 
-	fmt.Println("stopped")
-	c.Started = false
-}
+	{
+		// Common
+		c.SetString("Common/Name", "", "stopped")
+		c.SetString("Common/ProcessID", "", "stopped")
 
-func GetProcesses() []ProcessInfo {
-	result := make([]ProcessInfo, 0)
-	return result
+	}
+
+	logger.Println("UNIT <Process Windows> stopped:", c.Id())
+	c.Started = false
 }
