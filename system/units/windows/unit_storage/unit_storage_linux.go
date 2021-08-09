278829b2f981af4cf0a81bf47a3cbe1a2530216a
ipoluyanov <ipoluyanov@gmail.com>
2021-08-09 08:49:54 +0300

    mac sync

--- system/units/windows/unit_storage/unit_storage_linux.go
+++ system/units/windows/unit_storage/unit_storage_linux.go
@@ -20,7 +20,7 @@ type UnitStorage struct {
 var Image []byte
 
 func init() {
-	Image = resources.R_files_sensors_sensor_windows_ram_png
+	Image = resources.R_files_sensors_category_network_png
 }
 
 func New() common_interfaces.IUnit {
