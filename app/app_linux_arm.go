21a9ed795ed75d3a382b7ba9a520b67819fec9d7
ipoluyanov <ipoluyanov@gmail.com>
2021-08-09 14:16:30 +0300

    mac sync

--- app/app_linux_arm.go
+++ app/app_linux_arm.go
@@ -4,10 +4,34 @@ import (
 	"fmt"
 	"github.com/gazercloud/gazernode/logger"
 	"github.com/gazercloud/gazernode/utilities/paths"
+	"github.com/stianeikeland/go-rpio/v4"
+	"time"
 )
+
+func ttt() {
+	err := rpio.Open()
+
+	if err != nil {
+		return
+	}
+
+	pin10 := rpio.Pin(10)
+	pin10.PullUp()
+	pin10.Input()
+
+	for {
+		st := pin10.Read()
+		time.Sleep(200 * time.Millisecond)
+		logger.Println("r", st)
+	}
+
+	rpio.Close()
+}
 
 func RunDesktop() {
 	logger.Init(paths.HomeFolder() + "/gazer/log_ui")
+	ttt()
+	return
 	start()
 	logger.Println("Started as console application")
 	logger.Println("Press ENTER to stop")
