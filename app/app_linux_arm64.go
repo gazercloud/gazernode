8a4dc22701ba41ade3be49e593c6e679593cb324
ip <ipoluyanov@gmail.com>
2021-09-24 19:23:22 +0300

    2.4.4

--- app/app_linux_arm64.go
+++ app/app_linux_arm64.go
@@ -2,13 +2,14 @@ package app
 
 import (
 	"fmt"
+	"github.com/gazercloud/gazernode/application"
 	"github.com/gazercloud/gazernode/logger"
 	"github.com/gazercloud/gazernode/utilities/paths"
 )
 
 func RunDesktop() {
 	logger.Init(paths.HomeFolder() + "/gazer/log_ui")
-	start()
+	start(application.ServerDataPathArgument)
 	logger.Println("Started as console application")
 	logger.Println("Press ENTER to stop")
 	_, _ = fmt.Scanln()
