4f8bbdcfae0713bb949bcc33b37067ac83b6b749
ip <ipoluyanov@gmail.com>
2021-11-05 14:37:26 +0300

    2.4.4

--- app/app_linux_386.go
+++ app/app_linux_386.go
@@ -1,29 +1,17 @@
 package app
 
 import (
-	"github.com/gazercloud/gazernode/forms/mainform"
+	"fmt"
+	"github.com/gazercloud/gazernode/application"
 	"github.com/gazercloud/gazernode/logger"
 	"github.com/gazercloud/gazernode/utilities/paths"
-	"github.com/gazercloud/gazerui/ui"
-	"github.com/gazercloud/gazerui/uiforms"
 )
 
 func RunDesktop() {
 	logger.Init(paths.HomeFolder() + "/gazer/log_ui")
-
-	if *runServerFlagPtr {
-		start()
-	}
-
-	ui.InitUISystem()
-
-	{
-		var form mainform.MainForm
-		uiforms.StartMainForm(&form)
-		form.Dispose()
-	}
-
-	if *runServerFlagPtr {
-		stop()
-	}
+	start(application.ServerDataPathArgument)
+	logger.Println("Started as console application")
+	logger.Println("Press ENTER to stop")
+	_, _ = fmt.Scanln()
+	stop()
 }
