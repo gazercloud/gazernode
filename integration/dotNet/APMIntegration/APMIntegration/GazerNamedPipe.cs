8f7544d913f8ae71fecb37aa6feb2657a700c373
ip <ipoluyanov@gmail.com>
2022-03-26 12:12:40 +0300

    refactoring

---
+++ integration/dotNet/APMIntegration/APMIntegration/GazerNamedPipe.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO.Pipes;
+using System.Text;
+
+namespace APMIntegration
+{
+    public class GazerNamedPipe : IDisposable
+    {
+        public GazerNamedPipe(string pipeName)
+        {
+            pipeName_ = pipeName;
+        }
+
+        private string pipeName_ = "";
+        private NamedPipeClientStream stream_ = null;
+
+        public void Write(string name, string value)
+        {
+            try
+            {
+                if (stream_ == null)
+                {
+                    stream_ = new NamedPipeClientStream(pipeName_);
+                    stream_.Connect(0);
+                }
+
+                if (stream_ != null)
+                {
+                    var bytes = Encoding.UTF8.GetBytes(string.Format("{0}={1}\r\n", name, value));
+                    stream_.Write(bytes, 0, bytes.Length);
+                }
+            }
+            catch (Exception ex)
+            {
+                if (stream_ != null)
+                {
+                    stream_.Close();
+                    stream_.Dispose();
+                    stream_ = null;
+                    //GC.Collect();
+                }
+                //Console.WriteLine("GazerPipe Error: {0}", ex);
+            }
+        }
+
+        public static void Write(string pipeName, string name, string value)
+        {
+            using (NamedPipeClientStream stream = new NamedPipeClientStream(pipeName))
+            {
+                try
+                {
+                    stream.Connect(0);
+                    var bytes = Encoding.UTF8.GetBytes(string.Format("{0}={1}\r\n", name, value));
+                    stream.Write(bytes, 0, bytes.Length);
+                    stream.Close();
+                }
+                catch (Exception ex)
+                {
+                    //Console.WriteLine("GazerPipe Error: {0}", ex);
+                }
+            }
+
+            //GC.Collect();
+        }
+
+        public void Dispose()
+        {
+            if (stream_ != null)
+            {
+                stream_.Close();
+                stream_.Dispose();
+                stream_ = null;
+                //GC.Collect();
+            }
+        }
+    }
+}
--- integration/dotNet/APMIntegration/APMIntegration/Program.cs
+++ integration/dotNet/APMIntegration/APMIntegration/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Pipes;
 using System.Linq;
 using System.Text;
+using System.Threading;
 
 namespace APMIntegration
 {
@@ -9,6 +11,37 @@ namespace APMIntegration
     {
         static void Main(string[] args)
         {
+            TestClassInstance();
+            //TestStaticMethod();
+        }
+
+        static void TestStaticMethod()
+        {
+            Random rnd = new Random();
+            while (true)
+            {
+                GazerNamedPipe.Write("q_debug_pipe", "paramStatic1", (rnd.Next() % 100).ToString());
+                GazerNamedPipe.Write("q_debug_pipe", "paramStatic2", (rnd.Next() % 100).ToString());
+                GazerNamedPipe.Write("q_debug_pipe", "paramStatic3", (rnd.Next() % 100).ToString());
+                Console.WriteLine("{0}", DateTime.Now.ToString());
+                Thread.Sleep(100);
+            }
+        }
+
+        static void TestClassInstance()
+        {
+            using (GazerNamedPipe gazerPipe = new GazerNamedPipe("q_debug_pipe"))
+            {
+                Random rnd = new Random();
+                while (true)
+                {
+                    gazerPipe.Write("param1", (rnd.Next() % 1000).ToString());
+                    gazerPipe.Write("param2", (rnd.Next() % 1000).ToString());
+                    gazerPipe.Write("param3", (rnd.Next() % 1000).ToString());
+                    Console.WriteLine("{0}", DateTime.Now.ToString());
+                    Thread.Sleep(100);
+                }
+            }
         }
     }
 }
